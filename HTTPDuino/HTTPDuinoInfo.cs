5a96cb5c5bb65fa85d2652cb74f848d8be0494be
benato.denis96 <benato.denis96@gmail.com>
2014-11-07 16:19:18 +0000

    Working on JSON

--- HTTPDuino/HTTPDuinoInfo.cs
+++ HTTPDuino/HTTPDuinoInfo.cs
@@ -7,18 +7,10 @@ namespace HTTPDuino
 {
     public class HTTPDuinoInfo
     {
-        public static HTTPDuino.JToken getInfo()
+        public static HTTPDuino.MicroJSON.JSON getInfo()
         {
-            HTTPDuino.JToken info = HTTPDuino.JsonHelpers.Parse("{\"version\":\"\", \"author\":\"\", \"license\":\"\", \"platform\":\"\", \"webServer\":\"HTTPDuino\"}");
-            HTTPDuino.JArray data = (HTTPDuino.JArray)info;
-            HTTPDuino.JObject dataCollection = (HTTPDuino.JObject)data[0];
-            dataCollection["version"] = "0.1a";
-            dataCollection = (HTTPDuino.JObject)data[1];
-            dataCollection["author"] = "Benato Denis";
-            dataCollection = (HTTPDuino.JObject)data[2];
-            dataCollection["license"] = "BSD";
-            dataCollection = (HTTPDuino.JObject)data[2];
-            dataCollection["platform"] = "Netduino Plus 2";
+            HTTPDuino.MicroJSON.JSON info = new HTTPDuino.MicroJSON.JSON();
+            info.AppendEntity("name", 54);
             return info;
         }
     }
