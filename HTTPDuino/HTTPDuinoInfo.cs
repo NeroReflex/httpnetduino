91b37784dbb6bcce63e8a0b5ad08b3915c960b86
benato.denis96 <benato.denis96@gmail.com>
2014-11-09 16:07:23 +0000

    Information update

--- HTTPDuino/HTTPDuinoInfo.cs
+++ HTTPDuino/HTTPDuinoInfo.cs
@@ -7,10 +7,31 @@ namespace HTTPDuino
 {
     public class HTTPDuinoInfo
     {
+        /// <summary>
+        /// The name of the application that is using this web server to share information
+        /// </summary>
+        public static string ApplicationName = string.Empty;
+
+        /// <summary>
+        /// Returns a JSON cointaining information about the HTTP web server
+        /// </summary>
+        /// <returns>information stored as a JSON</returns>
         public static HTTPDuino.MicroJSON.JSON getInfo()
         {
+            //create the empty JSON that will be filled
             HTTPDuino.MicroJSON.JSON info = new HTTPDuino.MicroJSON.JSON();
-            info.AppendEntity("name", 54);
+            
+            //server related information
+            info.AppendEntity("server", "HTTPNetDuino");
+            info.AppendEntity("version", "0.1a");
+            info.AppendEntity("project-home", "https://code.google.com/p/httpnetduino/");
+            info.AppendEntity("author", "Benato Denis");
+
+            //usage related information
+            info.AppendEntity("host", Microsoft.SPOT.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()[0].IPAddress);
+            info.AppendEntity("application", HTTPDuinoInfo.ApplicationName);
+
+            //returns information
             return info;
         }
     }
